f2257429e7d340c5ed0edeeb2326e52182d8f9fa
kevin-rodri <kr399880@gmail.com>
2022-03-26 19:05:40 -0400

    forgot to save the changes I made before committing...

--- src/ParkingAPI.java
+++ src/ParkingAPI.java
@@ -2,15 +2,30 @@ import java.util.Random;
 
 public class ParkingAPI {
 
+	public ParkingAPI() {
 
-    public ParkingAPI() {
+	}
 
-    }
+	public int getOpenSpots(ParkingLot parkingLot) {
+		Random random = new Random(parkingLot.ordinal());
+		return random.nextInt(110);
+	}
+	
+	public int getFilledSpots(ParkingLot parkingLot) {
+		Random random = new Random(parkingLot.ordinal());
+		return random.nextInt(110);
+	}
 
-    public int getOpenSpots(ParkingLot parkingLot) {
-        Random random = new Random(parkingLot.ordinal());
-        return random.nextInt(110);
-    }
-    
-    
+	public ParkingLot availableParkingLots(ParkingLot one, ParkingLot two) {
+		int availableLotOne = getOpenSpots(one) - getFilledSpots(one);
+		int availableLotTwo = getOpenSpots(two) - getFilledSpots(two);
+		if (availableLotOne > availableLotTwo) {
+			return one;
+		} else if (availableLotOne < availableLotTwo) {
+			return two;
+		} else {
+			System.out.println("Both lots are full..");
+			return null;
+		}
+	}
 }
