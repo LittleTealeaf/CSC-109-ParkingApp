4e42e448f2a809134787aec810f2742c5cf2925c
SJGriffin21 <sjgriffin@quinnipiac.edu>
2022-04-02 11:14:13 -0400

    Added user interface

--- src/MostOpenLot.java
+++ src/MostOpenLot.java
@@ -1,19 +1,40 @@
+import java.util.Scanner;
 
 public class MostOpenLot {
 
 	public static void main(String[] args) {
 		ParkingAPI parkingAPI = new ParkingAPI();
 		
+		Scanner input = new Scanner(System.in);
+		String response = "";
+		boolean chosen = false;
+		System.out. println("Hello, would you like to see the parking lot with the most current open spaces or predicted open spaces?"
+				+ "\nPlease enter current or predicted");
 		
-		System.out.println(
-		parkingAPI.availableParkingLots(
-				parkingAPI.availableParkingLots(ParkingLot.WHITTNEY_LOT, ParkingLot.NORTH_LOT), 
-				ParkingLot.SOUTH_LOT));
+		while(!chosen) {
+			response = input.nextLine();
+			if (response.equalsIgnoreCase("current")) {
+				System.out.println("The parking lot with the most empty spaces is " + 
+						parkingAPI.availableParkingLots(
+						parkingAPI.availableParkingLots(ParkingLot.WHITTNEY_LOT, ParkingLot.NORTH_LOT), 
+						ParkingLot.SOUTH_LOT));
+				chosen = true;
+			}
+			else if (response.equalsIgnoreCase("predicted")) {
+				System.out.println("The parking lot with the most predicted empty spaces is " + 
+						parkingAPI.predictedOpenSpots(
+						parkingAPI.predictedOpenSpots(ParkingLot.NORTH_LOT,ParkingLot.SOUTH_LOT), 
+						ParkingLot.WHITTNEY_LOT)
+						);
+				chosen = true;
+			}
+			else {
+				System.out.println("Inavlid response please try again");
+			}
+		}
 		
-		System.out.println(parkingAPI.predictedOpenSpots(
-				parkingAPI.predictedOpenSpots(ParkingLot.NORTH_LOT,ParkingLot.SOUTH_LOT), 
-				ParkingLot.WHITTNEY_LOT)
-				);
+		
+
 		
 	}
 }
